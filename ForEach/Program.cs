808eb9137295c609294e9be1df10c837e01d61fb
SabirA <sabirsas@code.edu.az>
2026-02-03 23:39:31 +0400

    for each first commit

---
+++ ForEach/Program.cs
@@ -0,0 +1,68 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		#region region1
+		//int[] numbers = [5,4,8,7];
+		//foreach (int number in numbers){
+		//	int fac = 1;
+		//	for (int j = 2; j <= number; j++)
+		//		fac *= j;
+		//	Console.Write($"{fac} ");
+		//}
+		#endregion
+
+		#region region2
+		//int[] numbers = [5, 4, 8, 7];
+		//int min = numbers[0];
+		//foreach (int number in numbers){
+		//	if (min > number)
+		//		min = number;
+		//}
+		//foreach (int number in numbers){
+		//	if (min == number)
+		//		continue;
+		//	Console.Write($"{number} ");
+		//}
+		#endregion
+
+		#region region3
+		string word = "kertenkele";
+
+
+
+
+		#endregion
+
+		#region region4
+		#endregion
+
+		#region region5
+		#endregion
+
+		#region region6
+		#endregion
+
+		#region region7
+		#endregion
+
+		#region region8
+		#endregion
+
+		#region region9
+		#endregion
+
+		#region region10
+		#endregion
+
+		#region region11
+		#endregion
+
+		#region region12
+		#endregion
+
+		#region region13
+		#endregion
+
+	}
+}
